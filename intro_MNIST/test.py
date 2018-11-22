5b9d46294925c6318bfc5bedfa26c6d53c87a010
lyMeiSEU <213173625@seu.edu.cn>
2018-11-22 19:00:44 +0800

    3D tensor

--- intro_MNIST/test.py
+++ intro_MNIST/test.py
@@ -1,3 +1,7 @@
+import os 
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='1' # 这是默认的显示等级，显示所有信息  
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='2' # 只显示 warning 和 Error   
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='3' # 只显示 Error
 import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 import tensorflow as tf
